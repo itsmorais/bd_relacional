0ad078df0c6e747fa14149c7cd7a0cfb56bbbdab
Michael Morais <michael_morais@Outlook.com.br>
2023-08-11 22:52:45 -0300

    Aula 2 concluida

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# bd_relacional
+## Aula 2
+
+### Utilizar via linux terminal:
+```
+sudo -i -u postgres
+```
+em seguida:
+```
+sudo psql
+```
+### DDL - Create DATABASE
+- Comando para criar um banco de dados
+```sql
+    CREATE DATABASE nomeDoBanco;
+```
+    - Parâmetros opcionais
+        - WITH
+        - OWNER =
+        - ENCODING =
+        - TABLESPACE =
+        - CONNECTION LIMIT =
+
+Para verificar quais os bancos de dados existentes(listar):
+```
+\l
+```
+
+Para se conectar a um banco de dados:
+```
+\c <nome_do_banco>
+```
+
+Para criar tabelas
+```sql
+create table usuario(nome varchar(50), idade integer);
+```
+
+Para visualizar as tabelas
+```
+\dt
+```
+
+Para descrever uma tabela
+```
+\d <nome_da_tabela>
+```
+
+Alterar tabelas:
+```sql
+ALTER TABLE cliente ADD COLUMN idade integer;
+```
---
+++ aula02.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE db_sistema_bancario;
+
+CREATE TABLE usuario(nome varchar(50), idade integer);
+
+CREATE TABLE conta(agencia integer, conta_corrente integer);
+
+CREATE TABLE status(ativo boolean);
+
+CREATE TABLE saldo(saldo_total float, credito float);
+
+CREATE TABLE negativado(cliente_negativo boolean);
+
+-- Alterando o tipo de uma coluna
+ALTER TABLE
+    saldo
+ALTER column
+    saldo_total TYPE real;
+
+-- DROPAR TABELAS QUE CRIEI
+DROP TABLE conta;
+
+DROP TABLE usuario;
+
+DROP TABLE status;
+
+DROP TABLE saldo;
+
+DROP TABLE negativado;
+
+CREATE TABLE cliente(
+    NOME_CLIENTE varchar(50),
+    CIDADE_CLIENTE varchar(50),
+    ENDERECO_CLIENTE varchar(50)
+);
+
+CREATE TABLE conta(
+    NUMERO_CONTA integer,
+    NOME_AGENCIA varchar(15),
+    SALDO real
+);
+
+CREATE TABLE emprestimo(
+    NUMERO_EMPRESTIMO integer,
+    NOME_AGENCIA varchar(15),
+    VALOR real
+);
+
+CREATE TABLE agencia(
+    NOME_AGENCIA varchar(15),
+    CIDADE_AGENCIA varchar(30),
+    DEPOSITOS integer
+);
+
+ALTER TABLE
+    cliente
+ADD
+    COLUMN idade integer;
+
+ALTER TABLE
+    RENAME COLUMN idade TO idade_usuario;
+
+ALTER TABLE
+    DROP COLUMN idade_usuario;
+
+ALTER TABLE
+    cliente rename to tblclient;
