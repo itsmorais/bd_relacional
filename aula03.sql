979a71c41af90af770dbcce84e7f58a53104b80b
Michael Morais <michael_morais@Outlook.com.br>
2023-08-19 17:43:45 -0300

    Continuacao aula3

--- aula03.sql
+++ aula03.sql
@@ -2,13 +2,23 @@ DROP DATABASE IF EXISTS bd_aula03;
 
 CREATE DATABASE bd_aula03;
 
-\c bd_aula03;
+\c bd_aula03 
+
+CREATE DOMAIN chk_status text check (
+    value = 'DISPONIVEL'
+    OR value = 'ALUGADO'
+);
+
+CREATE DOMAIN chk_categoria text check (
+    value = 'DRAMA'
+    OR value = 'COMEDIA'
+);
 
 CREATE TABLE tbl_cliente(
-    codigo_cliente integer PRIMARY KEY,
-    Nome text NOT NULL,
-    Cidade text,
-    Endereco text
+    codigo_cliente integer NOT NULL PRIMARY KEY,
+    nome text NOT NULL,
+    cidade text,
+    endereco text
 );
 
 CREATE TABLE tbl_emprestimo(
@@ -18,13 +28,13 @@ CREATE TABLE tbl_emprestimo(
 );
 
 CREATE TABLE tbl_titulo(
-    codigo_titulo integer PRIMARY KEY,
+    codigo_titulo integer NOT NULL PRIMARY KEY,
     descricao text,
-    categoria text
+    categoria chk_categoria
 );
 
 CREATE TABLE tbl_livros(
     cod_livro integer PRIMARY KEY,
-    codigo_titulo text,
-    status text
+    codigo_titulo integer references tbl_titulo(codigo_titulo),
+    status text DEFAULT 'DISPONIVEL' :: chk_status
 );
